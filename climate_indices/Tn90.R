e163a801ced00a0e794afb06bbb39eafcb07c1ac
Michael Stölzle <13642619+modche@users.noreply.github.com>
2018-01-18 11:29:00 +0100

    Update Tn90.R
    New function with updates provided.

--- climate_indices/Tn90.R
+++ climate_indices/Tn90.R
@@ -1,37 +1,61 @@
-# Function to calculate indicator 194 "Tn90"
-
-Tn90 <- function(date, Tmin) {
+Tn90 <- function(x,y){            
+  # function to calculate indicator 194 "Tn90"
+  # x = Tmin
+  # y = date
   
-  require(lubridate)
+  # packages required
   require(tidyverse)
+  require(lubridate)
+ 
+# ----- breaks -----
+  if(length(y) != length(x)) {
+    stop("y and x must be the same length")
+  }
+    
+    
+  if(!is.numeric(x)){
+    stop("x must be 'numeric'")
+  }
   
-  if(length(date) != length(Tmin))
-    stop("'date' and 'Tmin' must be the same length")
+  if(!is.Date(y)) {
+    stop("y must be 'date'")
+  }   
   
-  if(is.Date(date)==F)
-    stop("'date' must be a date")
-  
-  if(is.numeric(Tmin)==F)
-    stop("'Tmin' must be numeric")
-  
-  BW <- data.frame(date, Tmin)
-  
-  BW$ny <- year(BW$date)
-  
-  BW <- BW %>% group_by(ny) %>% filter(sum(is.na(Tmin))<60)
-  
-  if(length(BW$ny)<10)
-    stop("sorry, not enough data for calculation (less than 10 years)")
-  
-  q <- quantile(BW$Tmin, .9, na.rm=T)
-  
-  BW_Tn90 <- BW %>% group_by(ny) %>% summarise(Tn90=length(which(Tmin>q))/length(Tmin)*100)
-  
-  AVGges <- mean(BW_Tn90$Tn90, na.rm = T)
-  
-  AVG1 <- BW_Tn90 %>% filter(ny>=1977, ny<1997) %>% summarise(AVG=mean(Tn90, na.rm=T))
-  AVG2 <- BW_Tn90 %>% filter(ny>=1997, ny<=2016) %>% summarise(AVG=mean(Tn90, na.rm=T))
-  rateAVG <- AVG2$AVG/AVG1$AVG
-  
-  return(list(year=BW_Tn90$ny, Tn90=BW_Tn90$Tn90, Average=AVGges, RateOfChange=rateAVG))
+# ----- prepare variables ------
+    df <- data.frame(x,y) 
+    
+    df$year <- year(df$y)
+    
+# ----- quality check -----   
+     df <- df %>% group_by(year) %>% filter(sum(is.na(x))<60)
+    
+# ----- analysis -----    
+    q <- quantile(df$x,0.9,na.rm=TRUE)
+    
+    Tn90df <- df  %>% group_by(year) %>% 
+      summarise(Tn90 = length(which(x > q))/length(x)*100)
+    
+    Tn90df2 <- Tn90df %>% filter(year >= 1997, year <= 2016)
+    
+    if(length(Tn90df2$year) < 10) {
+      stop("length(y) < 10")
+    }
+    
+    Tn90df2 <- Tn90df2 %>% summarise(AVG1 = mean(Tn90, na.rm=TRUE))
+    
+    Tn90df3 <- Tn90df %>% filter(year >= 1977, year <= 1996)
+    
+    if(length(Tn90df3$year) < 10) {
+      stop("length(y) < 10")
+    }
+ 
+    Tn90df3 <- Tn90df3 %>% summarise(AVG2 = mean(Tn90, na.rm=TRUE))
+    
+    avg <- mean(Tn90df$Tn90, na.rm = TRUE)
+      
+    roc <- Tn90df2$AVG1/Tn90df3$AVG2
+    
+# ----- return -----   
+    list <- list(year = Tn90df$year, Tn90 = Tn90df$Tn90, avg = avg, rate_of_change = roc)
+    return(list)
 }
