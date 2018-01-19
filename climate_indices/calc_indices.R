8c9337041d2dedd3e8cf063b93111d54b5a517c0
siggi <35223372+steezbert@users.noreply.github.com>
2018-01-19 12:10:35 +0100

    Update calc_indices.R
    added data perparation to run functions.
    added result grab from functions (still issues with the grab of ratio from R10 function. needs to be adjusted in the function itself or in this script).
    to run this script you ahve to load functions into R first.

--- climate_indices/calc_indices.R
+++ climate_indices/calc_indices.R
@@ -1,48 +1,97 @@
 library(tidyverse)
+library(readr)
 
 # ----- Station id-----
-id <- tibble(id = 9999)
+station_id <- 755
+id <- tibble(id = station_id)
+
+# ----- data path-----
+path <- "C:/Users/user/Desktop/Data_Management/BW_Climate_1977_2016.txt"
+
+# ----- running functions ----- 
+#CDD
+BW_C <- read_tsv(path)
+CDD_result <- CDD(BW_C, station_id)
+
+#ETR
+ETR_result <- ETR(BW_C, station_id)
+
+#Fd
+fd_result <- fun_frostdays(BW_C, station_id)
+
+#R10
+r10_result <- R10(BW_C, station_id)
+
+#R5d
+r5d_result <- calc_R5d(BW_C, station_id)
+
+#R95T
+r95t_result <- R95T(BW_C, station_id)
+
+#SDII
+SDII_result <- SDII(BW_C, station_id)
+
+#Tn90
+data <- read.csv(path, sep = "\t", dec = ".", header = TRUE)
+data$as_date <- as.Date(data$date, format = "%Y-%m-%d")
+station <- subset(data[which(data$id == station_id),])
+Tn90_result <- Tn90(station$TNK, station$as_date)
+
+#GSL
+data <- read.csv(path, sep = "\t", dec = ".", header = TRUE)
+data$as_date <- as.Date(data$date, format = "%Y-%m-%d")
+station <- subset(data[which(data$id == station_id),])
+GSL_result <- GSL(station$as_date, station$TMK)
+
+#HWDI
+data <- read.csv(path, sep = "\t", dec = ".", header = TRUE)
+data$as_date <- as.Date(data$date, format = "%Y-%m-%d")
+HWDI_result <- HWDI(data, station_id)
+
+
+
 
 # ----- Template to calculate the indices for one station -----
-avg_Fd    <- NA
-ratio_Fd  <- NA
+avg_Fd    <- as.numeric(fd_result[1])
+ratio_Fd  <- as.numeric(fd_result[2])
 
-avg_ETR    <- NA
-ratio_ETR  <- NA
+avg_ETR    <- as.numeric(ETR_result[1])
+ratio_ETR  <- as.numeric(ETR_result[2])
 
-avg_GSL    <- GSL(date,temp)$average
-ratio_GSL  <- GSL(date,temp)$ratio
+avg_GSL    <- as.numeric(GSL_result[1])
+ratio_GSL  <- as.numeric(GSL_result[2])
 
-avg_HWDI    <- NA
-ratio_HWDI  <- NA
+avg_HWDI    <- as.numeric(HWDI_result[1][1])
+ratio_HWDI  <- as.numeric(HWDI_result[2][1])
 
-avg_Tn90    <- NA
-ratio_Tn90  <- NA
+avg_Tn90    <- as.numeric(Tn90_result[3])
+ratio_Tn90  <- as.numeric(Tn90_result[4])
 
-avg_R10    <- NA
-ratio_R10  <- NA
+avg_R10    <- as.numeric(r10_result[2])
+ratio_R10  <- as.numeric(r10_result[3])
 
-avg_CDD    <- NA
-ratio_CDD  <- NA
+avg_CDD    <- as.numeric(CDD_result[2])
+ratio_CDD  <- as.numeric(CDD_result[3])
 
-avg_R5d    <- NA
-ratio_R5d  <- NA
+avg_R5d    <- as.numeric(r5d_result[2])
+ratio_R5d  <- as.numeric(r5d_result[3])
 
-avg_SDII    <- NA
-ratio_SDII  <- NA
+avg_SDII    <- as.numeric(SDII_result[2])
+ratio_SDII  <- as.numeric(SDII_result[1])
 
-avg_R95T    <- NA
-ratio_R95T  <- NA
+avg_R95T    <- as.numeric(r95t_result[1])
+ratio_R95T  <- as.numeric(r95t_result[2])
 
 
 # ----- Combine the indices -----
 avgs <- tibble(avg_Fd, avg_ETR, avg_GSL, avg_HWDI,
-       avg_Tn90, avg_R10, avg_CDD, avg_R5d,
-       avg_SDII, avg_R95T)
+               avg_Tn90, avg_R10, avg_CDD, avg_R5d,
+               avg_SDII, avg_R95T)
 
 ratios <- tibble(ratio_Fd, ratio_ETR, ratio_GSL, ratio_HWDI,
-               ratio_Tn90, ratio_R10, ratio_CDD, ratio_R5d,
-               ratio_SDII, ratio_R95T)
+                 ratio_Tn90, ratio_R10, ratio_CDD, ratio_R5d,
+                 ratio_SDII, ratio_R95T)
 
 # ----- Should have the same order as the GoogleSheet to copy&paste-----
 result <- bind_cols(id, avgs,ratios)
+View(result)
